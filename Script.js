12ca619e95d3b656839dcdc0d3f03880a31d6e39
guypridan <guy.pridan@gmail.com>
2023-01-23 21:30:23 +0200

    'getting better at git?'

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
-let insertString = "208506113";
-
-let input = document.getElementById("p_mis_student");
-input.focus().val(insertString)
+(() => {
+    let insertString = '208506113';
+    if (typeof document.getElementsByName("p_mis_student") != 'undefined') {
+        let input = document.getElementById("p_mis_student").value = insertString;
+    }
+})();
